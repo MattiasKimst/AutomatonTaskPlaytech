dbfdd01f46915206d12bdcbb063f4161909b8061
MattiasKimst <mattias.kimst@ut.ee>
2024-03-26 07:42:11 +0200

    webdriver, visit playtech.ee

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //starting a session
+        WebDriver driver = new ChromeDriver();
+
+        //visiting playtech.ee
+        driver.get("https://www.playtech.ee");
     }
 }
