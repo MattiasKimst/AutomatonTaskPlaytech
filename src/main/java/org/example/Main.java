ff10225117eef42fee581e76a2fcb91a80543ff1
MattiasKimst <mattias.kimst@ut.ee>
2024-03-26 08:59:23 +0200

    use coordinates to click on "Who we are"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class Main {
@@ -17,8 +19,11 @@ public class Main {
         // Maximize the browser window
         driver.manage().window().maximize();
 
-        // locate <a> element that contains text "Who we are" and click on it
-        driver.findElement(By.xpath("//a[contains(text(),'Who we are')]")).click();
+        // Locate the element using coordinates
+        WebElement whoWeAreTab = driver.findElement(By.xpath("//a[contains(text(),'Who we are')]"));
+        Actions actions = new Actions(driver);
+        // Move to the element and click at the given coordinates
+        actions.moveToElement(whoWeAreTab, 1, 1).click().perform();
 
         //verify if page contains "Global presence"
         boolean isGlobalPresenceShown = driver.getPageSource().contains("Global presence");
