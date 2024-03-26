f854046d4b0a32cd55ac9add149a177eadda63a8
MattiasKimst <mattias.kimst@ut.ee>
2024-03-26 08:07:35 +0200

    find <a> containing "Who we are", click on it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
 public class Main {
     public static void main(String[] args) {
+
 
         //starting a session
         WebDriver driver = new ChromeDriver();
 
         //visiting playtech.ee
         driver.get("https://www.playtech.ee");
+
+        // Maximize the browser window
+        driver.manage().window().maximize();
+
+        // locate <a> element that contains text "Who we are" and click on it
+        driver.findElement(By.xpath("//a[contains(text(),'Who we are')]")).click();
+
     }
-}
+
+
+    }
